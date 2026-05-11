8da1834213d7f46af8a3fbc9b41385f1aed525d3
Shus13 <sushitkarki234@gmail.com>
2026-05-11 17:25:01 +0545

    components and props

---
+++ src/Button.jsx
@@ -0,0 +1,10 @@
+
+const Button = (props) => {
+  return (
+    <>
+        <button style={{background:'green',color:'white',padding:'20px',margin:'5px'}}>{props.text}</button>
+    </>
+  )
+}
+
+export default Button
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom"
+import Button from "./Button"
+import Card from "./Card"
 
 const Home = () => {
   return (
     <>
-    <nav>
+    {/* <nav>
         <Link to="/">Home</Link>
         <Link to="/about" >About</Link>
         <Link to="/contact">Contact</Link>
-    </nav>
+
+    </nav> */}
+
+    <Button text='Register'/>
+    <Button text='Login'/>
+    <Button text='Logout'/>
+    <Card title='Sushit' description='Hello, This is sushit, student at Tribhuvan University'/>
+    <Card title='Ram' description='Hello, I am Ram, professor at Pokhara University'/>
     </>
   )
 }
