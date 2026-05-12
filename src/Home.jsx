afd05c750672ae871d493a268abbf04cc4a1ecab
Shus13 <sushitkarki234@gmail.com>
2026-05-12 08:01:57 +0545

    useState hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,7 +9,6 @@ const Home = () => {
         <Link to="/">Home</Link>
         <Link to="/about" >About</Link>
         <Link to="/contact">Contact</Link>
-
     </nav> */}
 
     <Button text='Register'/>
