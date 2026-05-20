c6c0e33538a68f92461913e7e6e007ea8a4c8aab
Shus13 <sushitkarki234@gmail.com>
2026-05-20 23:27:48 +0545

    edit page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import SingleBlog from './pages/SingleBlog';
 import CreateBlog from './pages/CreateBlog';
+import EditPage from './pages/EditPage';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/create' element={<CreateBlog />}/>
         <Route path='/blog/:id' element={<SingleBlog />}/>
+        <Route path='edit/:id' element={<EditPage />}/>
       </Routes>
     </BrowserRouter>
     </>
--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
 import axios from "axios"
-import { useNavigate, useParams, useSearchParams } from "react-router-dom"
+import { Link, useNavigate, useParams, useSearchParams } from "react-router-dom"
 
 const SingleBlog = () => {
 
@@ -45,7 +45,9 @@ const SingleBlog = () => {
         </div>
         <div className="flex -mx-2 mb-4">
           <div className="w-1/2 px-2">
+          <Link to={`/edit/${blog._id}`}> 
             <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit Blog</button>
+          </Link>
           </div>
           <div className="w-1/2 px-2">
             <button className="w-full bg-red-600 dark:bg-red-600 text-white py-2 px-4 rounded-full font-bold hover:bg-red-800 dark:hover:bg-gray-600" onClick={deleteFunction}>Delete Blog</button>
